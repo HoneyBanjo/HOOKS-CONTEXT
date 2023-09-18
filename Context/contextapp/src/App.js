25b9e1a891cf70009ecb0625da1734adcca239e9
Benjamin Nousiainen <b.nousiainen@gmail.com>
2023-09-18 17:12:10 +0300

    Combination app

--- Context/contextapp/src/App.js
+++ Context/contextapp/src/App.js
@@ -4,6 +4,7 @@ import BookList from './components/BookList'
 import ThemeContextProvider from './contexts/ThemeContext';
 import ThemeToggle from './components/ThemeToggle';
 import AuthContextProvider from './contexts/AuthContext';
+import BookContextProvider, { BookContext } from './contexts/BookContext';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       <ThemeContextProvider>
         <AuthContextProvider>
           <Navbar />
+          <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
           <BookList />
           <ThemeToggle />
         </AuthContextProvider>
